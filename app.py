d9d39ab52b18f178bde19220b5a381b950657f64
Subhajit Chakraborty <subhajitchakraborty509867@gmail.com>
2025-07-26 04:50:41 +0530

    2

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from google import genai
 from flask_cors import CORS
 from datetime import datetime
+import os
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 API_KEY = ""
 CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
 client = None
@@ -166,4 +168,4 @@ def giveFeedback():
     return jsonify({"response": feedback.text})
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=port)
